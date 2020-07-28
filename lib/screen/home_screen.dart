a34854965d4849143c996a71fbc20b1143ebfbb4
pjs3065 <tmakdlfwotjd@naver.com>
2020-07-28 11:25:19 +0900

    넷플릭스 홈 스크린 상단 Row 구현

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -22,13 +22,36 @@ class TopBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: EdgeInsets.fromLTRB(20, 7, 20, 7),
-      child: Row(children: <Widget>[
-        Image.asset(
-          'images/bbongflix_logo.png',
-          fit: BoxFit.contain,
-          height: 25,
-        ),
-      ]),
+      child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Image.asset(
+              'images/bbongflix_logo.png',
+              fit: BoxFit.contain,
+              height: 25,
+            ),
+            Container(
+              padding: EdgeInsets.only(right: 1),
+              child: Text(
+                'TV 프로그램',
+                style: TextStyle(fontSize: 14),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(right: 1),
+              child: Text(
+                '영화',
+                style: TextStyle(fontSize: 14),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.only(right: 1),
+              child: Text(
+                '내가 찜한 콘텐',
+                style: TextStyle(fontSize: 14),
+              ),
+            ),
+          ]),
     );
   }
 }
